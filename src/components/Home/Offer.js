1eaae29403ee9ab61c8be36ecf573aab7949b561
nazibhaque137 <nazibhaque137@gmail.com>
2022-05-29 00:35:50 +0600

    extra section offer created

---
+++ src/components/Home/Offer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Offer = () => {
+    return (
+        <div className="text-center my-20">
+            <h1 className='text-4xl font-bold uppercase m-5'>Stay Tuned to get exiting offers!</h1>
+        </div>
+    );
+};
+
+export default Offer;
