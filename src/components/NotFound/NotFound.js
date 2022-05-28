c39786c35e7c2e6b06191afdafb6ef1260a1a899
nazibhaque137 <nazibhaque137@gmail.com>
2022-05-29 00:35:01 +0600

    image added on NotFound page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import errorNotFound from '../../images/errorNotFound.jpg';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className="items-center justify-center">
+            <img className="h-50 w-50" alt="not-found-img" src={errorNotFound}/>
             <h2>404 error! The page you're looking for cannot be found!</h2>
         </div>
     );
