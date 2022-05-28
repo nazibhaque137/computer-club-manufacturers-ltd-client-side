8186868fd7582b044f37f03a4d2a42a3369b28b3
nazibhaque137 <nazibhaque137@gmail.com>
2022-05-29 00:40:57 +0600

    card updated for showing items

--- src/components/Home/Item.js
+++ src/components/Home/Item.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, useNavigate} from 'react-router-dom';
-import './Item.css';
 
 const Item = ({ item }) => {
     const { _id, name, image, short_description, min_order_quantity, available_quantity, price_per_unit } = item;
@@ -12,21 +11,23 @@ const Item = ({ item }) => {
     }
 
     return (
-        <div className='item-container'>
-            <img className='w-100 item-img' src={image} alt="" />
-
-            <h2>{name}</h2>
-            <p>BDT {price_per_unit}</p>
-            <p><small>{short_description}</small></p>
-            <p>Min Order Quantity: {min_order_quantity}</p>
-            <p>Available Quantity: {available_quantity}</p>
-        {/*
-            <Link to={`/purchase/${_id}`}>
-            <button disabled={available_quantity === 0} className='btn btn-primary purchase-btn'>Purchase: {name}</button>
-            </Link>
-    */}
-            <button onClick={() => navigateToPurchase(_id)} className='btn btn-primary'>Purchase: {name}</button>
-        </div> 
+        <div className='grid place-items-center'>
+        <div class="card w-96 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img alt="items-img" src={image} class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+                <h2 class="card-title">{name}</h2>
+                <h4>BDT {price_per_unit}</h4>
+                <p>{short_description}</p>
+                <p>Min Order Quantity: {min_order_quantity}</p>
+                <p>Available Quantity: {available_quantity}</p>
+    <div class="card-actions">
+        <button onClick={() => navigateToPurchase(_id)} className='btn btn-primary'>Purchase: {name}</button>
+    </div>
+  </div>
+</div>
+</div>
     );
 };
 
